f7e2732b81d8dc5c94a139f957978582a9a60581
0xamhi <saifmahi89@gmail.com>
2022-10-09 13:03:41 +0600

    newsapi

---
+++ index.js
@@ -0,0 +1,29 @@
+const express  = require('express')
+const axios = require('axios')
+const PORT = 6969 || process.env.PORT
+const cheerio = require('cheerio')
+const { response } = require('express')
+const app = express()
+const arts = []
+app.get('/' , (req,res)=>{
+    res.json('runing')
+})
+
+app.get('/news',(req,res)=>{
+    axios.get('https://www.prothomalo.com/collection/latest').then((response)=>{
+        const html = response.data
+        const $ = cheerio.load(html)
+        $('a',html).each(function(){
+            const title = $(this).text()
+            const url = $(this).attr('href')
+            arts.push({
+                title,
+                url
+            })
+        })
+        res.json(arts)
+    }).catch((err) => console.log(err))
+})
+
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
